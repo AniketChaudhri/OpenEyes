57ed545ab38088855fed328287bcee43857fd3a2
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-07-08 23:07:43 +0530

    Update Home.py

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -16,3 +16,32 @@ st.info(
     """👈 Go to the chat section to talk to the bot!
 """
 )
+
+with st.expander("💡 Idea"):
+    st.markdown(
+        """
+        - The idea behind this project is to create a chatbot that can answer questions related to animals.
+        - The chatbot is trained on the [AnimalQA dataset](
+        https://www.kaggle.com/datasets/iamsouravbanerjee/animal-image-dataset-90-different-animals
+        ) from Kaggle.
+        We thought that this app will help the tourists who are visiting safari parks and zoos and want to know more about the animals they see.
+        They can scan the animals from their phones and our Object Detection model will detect the animal and then the chatbot will answer the questions related to the animal.
+        """
+    )
+
+with st.expander("🐍 Python Libraries Used"):
+    st.markdown(
+        """
+        - `Streamlit` - For the frontend
+        - `OpenAI` - For the chatbot
+        - `Requests` - For making HTTP requests
+        - `Time` - For adding delays
+        - `Base64` - For encoding and decoding
+        - `JSON` - For parsing JSON
+        - `Pillow` - For image processing
+        - `Numpy` - For numerical processing
+        - `Pandas` - For data processing
+        - `Matplotlib` - For data visualization
+        - `YOLOv8` - For object detection
+        """
+    )

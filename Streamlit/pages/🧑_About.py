6be9379994abb6efd9244e4f09c76ba97770e81b
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-07-08 21:45:46 +0530

    Update 🧑_About.py

--- Streamlit/pages/🧑_About.py
+++ Streamlit/pages/🧑_About.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 
 
 st.set_page_config(
@@ -9,6 +8,7 @@ st.set_page_config(
 
 TeamMember1 = {
     "Name": "Adarsh Anand",
+    "image": "https://avatars.githubusercontent.com/u/73928744?v=4",
     "Role": "DSC Lead '22 | Foss Overflow Mentor | Postman Leader | MERN | 4⭐CC | IIT Goa CSE '24",
     "LinkedIn": "https://www.linkedin.com/in/adarsh-anand-iitgoa/",
     "GitHub": "https://github.com/adarshanand67",
@@ -17,6 +17,7 @@ TeamMember1 = {
 
 TeamMember2 = {
     "Name": "Aniket Akshay Chaudhri",
+    "image": "https://avatars.githubusercontent.com/u/79798301?v=4",
     "Role": "Head @ Coding Club IIT Goa | Competitive Programmer | Web Developer | Android Developer | CSE @ IIT Goa",
     "LinkedIn": "https://www.linkedin.com/in/aniketchaudhri/",
     "GitHub": "https://github.com/AniketChaudhri/",
@@ -28,23 +29,24 @@ TeamMember2 = {
 
 # layout 2 cols
 st.title("About OpenEyes")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns([3,1,3])
+
+# center text
+# st.markdown("<h1 style='text-align: center; color: black;'>Meet the Team</h1>", unsafe_allow_html=True)
 
 # create 2 cards showing the details
-with col1:
-    # display image located at ../adarsh.jpg
-    # st.image(adarsh, width=200,caption="Adarsh Anand")
-    st.header(TeamMember1["Name"])
-    st.markdown(TeamMember1["Role"])
-    st.markdown(f"[LinkedIn]({TeamMember1['LinkedIn']})")
-    st.markdown(f"[GitHub]({TeamMember1['GitHub']})")
-    st.write(TeamMember1["description"])
 
-with col2:
-    # st.image(TeamMember2["image"], width=200)
-    # st.image(aniket, width=200,caption="Aniket Akshay Chaudhri")
-    st.header(TeamMember2["Name"])
-    st.markdown(TeamMember2["Role"])
-    st.markdown(f"[LinkedIn]({TeamMember2['LinkedIn']})")
-    st.markdown(f"[GitHub]({TeamMember2['GitHub']})")
-    st.write(TeamMember2["description"])
+def show_details(TeamMember):
+    st.image(TeamMember["image"], width=200)
+    st.header(TeamMember["Name"])
+    st.markdown(TeamMember["Role"])
+    st.markdown(f"[LinkedIn]({TeamMember['LinkedIn']})")
+    st.markdown(f"[GitHub]({TeamMember['GitHub']})")
+    st.write(TeamMember["description"])
+
+# add border
+with col1:
+    show_details(TeamMember1)
+
+with col3:
+    show_details(TeamMember2)

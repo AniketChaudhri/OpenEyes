bb25fab1d1375d62e17b34a48354f6b062b251ad
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-07-08 21:51:07 +0530

    Default state of chatgpt

--- Streamlit/pages/✉_Chat.py
+++ Streamlit/pages/✉_Chat.py
@@ -4,8 +4,6 @@ import requests
 import time
 
 st.set_page_config(
-    # page_title="Streamlit Demo",
-    # page_icon=":smiley:",
     page_title="OpenEyes",
     page_icon="👀",
 )
@@ -14,6 +12,7 @@ st.set_page_config(
 st.title("OpenEyes")
 
 API_KEY = st.secrets["OPENAI_API_KEY"]
+
 
 def makeRequest(prompt):
     url = "http://3.88.181.187:8080/v1/"
@@ -25,15 +24,23 @@ def makeRequest(prompt):
     response = requests.post(url, headers=headers, json=data)
     return response.json()
 
+
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4" # setting up a session state model
+    st.session_state["openai_model"] = "gpt-4"  # setting up a session state model
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [] # setting up a session state messages to store the messages
+    # st.session_state.messages = [] # setting up a session state messages to store the messages
+    # set a default message hi to the bot before the user types anything
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hi, I'm OpenEyes. I'm here to help you with your queries related to Animals. What do you want to know?",
+        }
+    ]
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]): # creating a chat message with the role
-        st.markdown(message["content"]) # adding the content to the chat message
+    with st.chat_message(message["role"]):  # creating a chat message with the role
+        st.markdown(message["content"])  # adding the content to the chat message
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
@@ -46,25 +53,6 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        # for response in openai.ChatCompletion.create(
-        # model=st.session_state["openai_model"],
-        # messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-        # stream=True,
-        # ):
-        #   full_response += response.choices[0].delta.get("content", "")
-        #   message_placeholder.markdown(full_response + "▌")
-        #   message_placeholder.markdown(full_response)
-        #   st.session_state.messages.append({"role": "assistant", "content": full_response})
-         # Simulate stream of response with milliseconds delay
-        #     for chunk in assistant_response.split():
-        #         full_response += chunk + " "
-        #         time.sleep(0.05)
-        #         # Add a blinking cursor to simulate typing
-        #         message_placeholder.markdown(full_response + "▌")
-        #     message_placeholder.markdown(full_response)
-        # # Add assistant response to chat history
-        # st.session_state.messages.append({"role": "assistant", "content": full_response})
-        # show the response with the text streaming from the api
         response = makeRequest(prompt)
         for chunk in response["choices"][0]["message"]["content"].split():
             full_response += chunk + " "
@@ -72,9 +60,8 @@ if prompt := st.chat_input("What is up?"):
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-        # st.session_state.messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": full_response}
+        )
 
-
-
+print(st.session_state.messages)

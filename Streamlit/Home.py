5287cbc587868bebd844dba2cdb82582caaf956b
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-07-08 21:45:50 +0530

    Update Home.py

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Streamlit Demo",
-    page_icon=":smiley:",
+    page_title="OpenEyes",
+    page_icon=":eyes:",
 )
 
-st.title("Streamlit Demo Homepage")
+st.title("OpenEyes")
+
+# Welcome message
+st.write("Welcome to the OpenEyes Homepage!")
+
+# goto the chat section to talk to the bot
+st.write("Go to the chat section to talk to the bot!")
